c032023fbe256615bbf8621367de78b354a55faa
Remon <remon_028@outlook.com>
2022-03-17 02:06:25 +0800

    Added ability to add/remove Projects, load tasks, add/remove tasks

--- src/index.js
+++ src/index.js
@@ -1,49 +1,279 @@
 import './style.css';
 
-const contentContainer = document.getElementById('content');
-
-
-const createTodo = (title, description, dueDate, priority) => {
-    return { title, description, dueDate, priority };
-}
-
-
-
-// NewTodoModal
-
-const newTodoCloseBtn = document.getElementById('newTodoCloseBtn');
-const newProjectCloseBtn = document.getElementById('newProjectCloseBtn');
-const newTodoModal = document.getElementById('newTodoModal');
-const newProjectModal = document.getElementById('newProjectModal');
-const openNewTodoModalBtn = document.getElementById('openNewTodoModalBtn');
-const openNewProjectModalBtn = document.getElementById('openNewProjectModalBtn')
-
-openNewTodoModalBtn.addEventListener('click', openTodoModal);
-
-//OPEN NEW TODO MODAL
-function openTodoModal() {
-    newTodoModal.style.display = 'block';
-}
-
-
-//CLOSE NEW TODO MODAL BY CLOSE BUTTON
-newTodoCloseBtn.addEventListener('click', closeTodoModal);
-
-function closeTodoModal() {
-    newTodoModal.style.display = 'none';
-}
-
-//CLOSE NEW TODO MODAL BY OUTSIDE CLICK
-window.addEventListener('click', outsideClick);
-
-function outsideClick(e) {
-    if (e.target === newTodoModal) {
-        newTodoModal.style.display = 'none';
-    } else if (e.target === newProjectModal) {
-        newProjectModal.style.display = 'none';
+//Project Class
+function createProject(title) {
+    return {
+        title,
+        tasks: [],
+        addTask(task) {
+            this.tasks.push(task);
+        },
+        deleteTask(el) {
+            el.parentElement.parentElement.remove();
+        },
+        deleteTaskFromList(taskTitle) {
+            allProjects.forEach((project) => {
+                if (activeProject.title === project.title) {
+                    project.tasks.forEach((task, index) => {
+                        if (task.title === taskTitle) {
+                            project.tasks.splice(index, 1);
+                        }
+                    })
+                }
+            })
+        }
     }
 }
 
+//Task Class
+function createTask(title, description, dueDate, priority) {
+    return {
+        title,
+        description,
+        dueDate,
+        priority,
+        completed: false
+    }
+}
+
+//todolist Class
+function todolist() {
+
+    return {
+        addProjectToList(project) {
+            const projectsList = document.getElementById('projectsList');
+
+            const projectBtn = document.createElement('li');
+            projectBtn.classList.add('project');
+
+            const projectBtnDiv = document.createElement('div');
+
+            const projectBtnIcon = document.createElement('span');
+            projectBtnIcon.innerText = 'list';
+            projectBtnIcon.classList.add('material-icons-outlined');
+            projectBtnDiv.appendChild(projectBtnIcon);
+
+            const projectTitle = document.createElement('span');
+            projectTitle.innerText = project.title;
+            projectBtnDiv.appendChild(projectTitle);
+
+            const projectDeleteBtn = document.createElement('span');
+            projectDeleteBtn.innerText = 'close';
+            projectDeleteBtn.classList.add('material-icons-outlined');
+            projectDeleteBtn.classList.add('delete');
+
+            projectBtn.appendChild(projectBtnDiv);
+            projectBtn.appendChild(projectDeleteBtn);
+
+            projectsList.appendChild(projectBtn);
+
+
+            projectBtnDiv.addEventListener('click', openProject);
+        },
+
+        deleteProject(el) {
+            el.parentElement.remove();
+        },
+
+        deleteProjectFromList(title) {
+
+            allProjects.forEach((project, index) => {
+                if (project.title === title) {
+                    allProjects.splice(index, 1);
+                }
+            })
+        }
+
+    }
+}
+
+const myTodolist = todolist();
+let activeProject;
+//Store Class
+
+let allProjects = [];
+//Event: Display Project;
+
+// document.addEventListener('DOMContentLoaded', myTodolist.displayProjects);
+
+//Event: Add Project
+document.getElementById('newProjectModal').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const title = document.getElementById('newProjectTitle').value;
+
+    const project = createProject(title);
+
+    myTodolist.addProjectToList(project);
+
+    allProjects.push(project);
+
+    closeProjectModal();
+})
+
+//Event: Opening a Project
+function openProject(e) {
+    e.stopPropagation();
+
+    activeProject = allProjects.filter(checkProjectTitle)[0];
+
+    function checkProjectTitle(project) {
+        return project.title === e.currentTarget.lastElementChild.innerText;
+    }
+
+    const projectTitle = document.getElementById('projectTitle');
+    projectTitle.innerText = activeProject.title;
+    const openNewTaskModalBtn = document.getElementById('openNewTaskModalBtn');
+    openNewTaskModalBtn.style.display = 'block';
+
+    renderTasks();
+}
+
+
+
+//Event: Add Task
+const openNewTaskModalBtn = document.getElementById('openNewTaskModalBtn');
+
+openNewTaskModalBtn.addEventListener('click', openTaskModal);
+
+
+document.getElementById('newTaskModal').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const tasksList = document.getElementById('tasksList');
+    clearElements(tasksList);
+
+    const newTaskTitle = document.getElementById('newTaskTitle');
+    const newTaskDescription = document.getElementById('newTaskDescription');
+    const newTaskDate = document.getElementById('newTaskDate');
+    const newTaskPriority = document.getElementById('newTaskPriority');
+
+
+    const newTask = createTask(newTaskTitle.value, newTaskDescription.value, newTaskDate.value, newTaskPriority.checked);
+
+
+    //add new task to project
+    allProjects.forEach((project) => {
+        if (activeProject.title === project.title) {
+            project.addTask(newTask);
+            activeProject = project;
+            console.log(allProjects);
+        }
+    })
+
+    renderTasks();
+})
+
+//render Tasks
+function renderTasks() {
+    const tasksList = document.getElementById('tasksList');
+    clearElements(tasksList);
+    activeProject.tasks.forEach(function (task) {
+        const taskEntry = document.createElement('li');
+        taskEntry.classList.add('task');
+
+        const taskLeftSide = document.createElement('div');
+        taskLeftSide.classList.add('taskLeftSide');
+
+        const checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        taskLeftSide.appendChild(checkbox);
+
+        const taskText = document.createElement('div');
+        taskText.classList.add('taskText');
+
+        const taskTitle = document.createElement('input');
+        taskTitle.type = 'text';
+        taskTitle.value = task.title;
+        taskTitle.setAttribute('readonly', true);
+        taskText.appendChild(taskTitle);
+
+        const taskDescription = document.createElement('input');
+        taskDescription.type = 'text';
+        taskDescription.value = task.description;
+        taskDescription.setAttribute('readonly', true);
+        taskText.appendChild(taskDescription);
+
+        taskLeftSide.appendChild(taskText);
+        taskEntry.appendChild(taskLeftSide);
+
+        const taskRightSide = document.createElement('div');
+        taskRightSide.classList.add('taskRightSide');
+
+        const taskDueDate = document.createElement('input');
+        taskDueDate.type = 'date';
+        taskDueDate.value = task.dueDate;
+        taskRightSide.appendChild(taskDueDate);
+
+        const editBtn = document.createElement('span');
+        editBtn.innerText = 'edit';
+        editBtn.classList.add('material-icons-outlined');
+        taskRightSide.appendChild(editBtn);
+
+        let priorityToggler = document.createElement('input');
+        priorityToggler.type = 'checkbox';
+        priorityToggler.classList.add('material-icons-outlined', 'starCheckbox');
+        if (task.priority) {
+            priorityToggler.checked = true;
+        } else {
+            priorityToggler.checked = false;
+        }
+
+        // priorityToggler.addEventListener('change', () => {
+        //     task.setPriority(priorityToggler.checked);
+        // })
+        taskRightSide.appendChild(priorityToggler);
+
+        const deleteBtn = document.createElement('span');
+        deleteBtn.innerText = 'delete';
+        deleteBtn.classList.add('material-icons-outlined');
+        deleteBtn.classList.add('delete');
+        taskRightSide.appendChild(deleteBtn);
+
+        taskEntry.appendChild(taskRightSide);
+
+        const tasksList = document.getElementById('tasksList');
+        tasksList.appendChild(taskEntry);
+
+        closeTaskModal();
+    });
+}
+
+
+//Event: Remove Project
+
+document.getElementById('projectsList').addEventListener('click', (e) => {
+    if (e.target.classList.contains('delete')) {
+        myTodolist.deleteProject(e.target);
+
+        myTodolist.deleteProjectFromList(e.target.previousElementSibling.lastElementChild.textContent);
+    }
+
+})
+
+//Event Remove Task
+
+document.getElementById('tasksList').addEventListener('click', (e) => {
+    if (e.target.classList.contains('delete')) {
+        activeProject.deleteTask(e.target);
+
+        activeProject.deleteTaskFromList(e.target.parentElement.previousElementSibling.lastElementChild.firstElementChild.value);
+        console.log(allProjects);
+    }
+})
+
+
+//Clean up tasks
+
+function clearElements(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+
+
+//New Project Modal
+const openNewProjectModalBtn = document.getElementById('openNewProjectModalBtn')
 
 openNewProjectModalBtn.addEventListener('click', openProjectModal);
 
@@ -51,8 +281,49 @@ function openProjectModal() {
     newProjectModal.style.display = 'block'
 }
 
+window.addEventListener('click', outsideClick);
+
+function outsideClick(e) {
+    if (e.target === newTaskModal) {
+        newTaskModal.style.display = 'none';
+        resetTaskForm();
+    } else if (e.target === newProjectModal) {
+        newProjectModal.style.display = 'none';
+    }
+}
+
+
+const newProjectCloseBtn = document.getElementById('newProjectCloseBtn');
+
 newProjectCloseBtn.addEventListener('click', closeProjectModal);
 
 function closeProjectModal() {
     newProjectModal.style.display = 'none';
+    newProjectTitle.value = '';
 }
+
+//New Task Modal
+
+
+const newTaskModal = document.getElementById('newTaskModal');
+
+function openTaskModal() {
+    newTaskModal.style.display = 'block';
+}
+
+const newTaskCloseBtn = document.getElementById('newTaskCloseBtn');
+
+newTaskCloseBtn.addEventListener('click', closeTaskModal);
+
+function closeTaskModal() {
+    newTaskModal.style.display = 'none';
+    resetTaskForm();
+}
+
+function resetTaskForm() {
+    newTaskTitle.value = "";
+    newTaskDescription.value = "";
+    newTaskDate.value = "";
+    newTaskPriority.checked = false;
+}
+
